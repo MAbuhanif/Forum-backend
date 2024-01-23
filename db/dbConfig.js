6c3773e3153fd97b7933852cf653016626d9c2a2
MAbuhanif <mundinoabete@gmail.com>
2024-01-23 21:42:34 +0000

    database

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,20 +1,21 @@
 const mysql2 = require("mysql2");
 
+// const dbConnection = mysql2.createPool({
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   host: process.env.LOCALHOST,
+//   password: process.env.PASSWORD,
+//   connectionLimit: 10,
+// });
+
+//for production
 const dbConnection = mysql2.createPool({
   user: process.env.USER,
   database: process.env.DATABASE,
-  host: "localhost",
+  host: process.env.LOCALHOST,
   password: process.env.PASSWORD,
   connectionLimit: 10,
 });
-
-// dbConnection.execute("select 'test'", (err, result) => {
-//   if (err) {
-//     console.log(err.message);
-//   } else {
-//     console.log(result);
-//   }
-// });
 
 module.exports = dbConnection.promise();
 
